657f29bb951bec0acf04468de65cf7ef7f898b53
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-06-22 10:01:33 +0200

    Creada extensions de Moq

---
+++ Extensions/Tests/Mocks/MoqExtension.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="MoqExtension.cs" company="PlaceholderCompany">
+//     Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Ricardo.CommonLibraries.Extensions.Tests.Mocks
+{
+    using global::Moq;
+
+    /// <summary>
+    ///     Moq extensions.
+    /// </summary>
+    public static class MoqExtension
+    {
+        /// <summary>
+        ///     Verify all and other calls.
+        /// </summary>
+        /// <typeparam name="T"> The mocked type. </typeparam>
+        /// <param name="mock"> The instance of <see cref="Mock{T}"/>. </param>
+        /// <returns> The <see cref="Mock{T}"/>. </returns>
+        public static Mock<T> VerifyAllAndOtherCalls<T>(this Mock<T> mock)
+            where T : class
+        {
+            mock.Verify();
+            mock.VerifyNoOtherCalls();
+
+            return mock;
+        }
+    }
+}
